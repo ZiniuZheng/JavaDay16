32797d2a2402ab5b875d7db54d30e028864b863f
ZiniuZheng <117133246+ZiniuZheng@users.noreply.github.com>
2024-07-21 23:21:31 +0800

    Create ExceptionDemo4.java

---
+++ ExceptionDemo4.java
@@ -0,0 +1,24 @@
+package exception;
+/*
+ * Throwable的常用方法：
+ * 		String getMessage()
+ * 		String toString()
+ * 		void printStackTrace()
+ */
+public class ExceptionDemo4 {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println(2 / 0);
+		} catch(ArithmeticException e) {
+			//String getMessage(): 原因
+			System.out.println(e.getMessage());
+			
+			//String toString(): 类型和原因
+			System.out.println(e.toString());
+			
+			//void printStackTrace(): 类型、原因和位置
+			e.printStackTrace();
+		}
+	}
+}
